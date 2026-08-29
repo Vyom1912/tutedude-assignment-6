f4242a2f152b3d4692f04bd9c76cae33017e4392
VYOM PATEL <vyom1912@gmail.com>
2026-08-29 12:59:44 +0530

    done all require instructions

---
+++ server.js
@@ -0,0 +1,88 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  if (req.url === "/" || req.url === "/home") {
+    const filePath = path.join(__dirname, "page", "home.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.writeHead(500, {
+          "content-type": "text/html",
+        });
+        res.end("<h1>500 - Internal Server Error</h1>");
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      res.end(data);
+    });
+  } else if (req.url === "/about") {
+    const filePath = path.join(__dirname, "page", "about.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.writeHead(500, {
+          "content-type": "text/html",
+        });
+        res.end("<h1>500 - Internal Server Error</h1>");
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      res.end(data);
+    });
+  } else if (req.url === "/contact") {
+    const filePath = path.join(__dirname, "page", "contact.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.writeHead(500, {
+          "content-type": "text/html",
+        });
+        res.end("<h1>500 - Internal Server Error</h1>");
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      res.end(data);
+    });
+  } else {
+    const filePath = path.join(__dirname, "page", "404.html");
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.writeHead(500, {
+          "content-type": "text/html",
+        });
+        res.end("<h1>500 - Internal Server Error</h1>");
+        return;
+      }
+
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+
+      res.end(data);
+    });
+  }
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
